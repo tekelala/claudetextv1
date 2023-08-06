e611ab3d8e67ca2caa5b26fb710bd1dc708793c3
tekelala <camilo@tekelala.com>
2023-08-06 17:03:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import streamlit as st
 from anthropic import Anthropic
 
-st.set_page_config(page_title="Anthropic Chat", page_icon=":robot:") 
+st.set_page_config(page_title="Anthropic Chat", page_icon=":robot:")
 
-anthropic = Anthropic(api_key=st.secrets["ANTHROPIC_API_KEY"])
+anthropic = Anthropic(api_key=st.secrets["ANTHROPIC_KEY"]) 
 
 if "generated_text" not in st.session_state:
-    st.session_state.generated_text = "" 
+    st.session_state.generated_text = ""
 
-if "conversation" not in st.session_state:
+if "conversation" not in st.session_state: 
     st.session_state.conversation = ""
 
 st.title("Anthropic Chat")
 
 user_input = st.text_input("You:", value="", key="input")
+
 if user_input:
     st.session_state.conversation += f"You: {user_input}\n"
 
+    # Simplify call
     response = anthropic.completions.create(
         model="claude-2",
-        prompt=st.session_state.conversation,
-        max_tokens=100,
-        stop_sequences=["Assistant:"]
+        prompt=st.session_state.conversation, 
+        max_tokens=100
     )
 
     st.session_state.generated_text = response.completion
